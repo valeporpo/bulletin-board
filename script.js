b7c501a20754135eb4a902ea4e55c3c08b34592a
Valerio <valerio.porpo@gmail.com>
2022-07-27 09:08:42 +0200

    switch to named function

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 let counter = 0
 let element = document.querySelector('body div.container')
-let animate = setInterval(function(){
+let animate = setInterval(translateToTheRight, 10)
+
+function translateToTheRight(){
   if(window.innerWidth == counter) {
     clearInterval(animate)
   }  
   element.style.transform = 'translate(-' + counter + 'px, 0)'
-  console.log(counter)
   counter++
-}, 10)
+}
