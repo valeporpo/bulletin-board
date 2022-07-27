5b617bed64d90df2751789511122dc4fa86c454a
Valerio <valerio.porpo@gmail.com>
2022-07-27 10:20:24 +0200

    Improved modularity and added clearInterval (bugfix)

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
-let counter = 0
+let counter = 0, animate
 let element = document.querySelector('body div.container')
 let elementWidth = element.offsetWidth
-counter = elementWidth
-element.style.transform = 'translateX(' + elementWidth + 'px)'
-let animate = setInterval(translateToLeft, 10)
+
+startAnimation()
 
 function translateToLeft() {
-  element.style.transform = 'translateX(' + counter + 'px)'
+  setTranslateX(counter)
   counter--
   if(counter < 0 && -window.innerWidth == counter) {
     goToRight()
-  }  
-
+  }
 }
 
 function goToRight() {
     clearInterval(animate)
-    element.style.transform = 'translateX(' + elementWidth+ 'px)'
+    setTranslateX(elementWidth)
     counter = elementWidth
     animate = setInterval(translateToLeft, 10)
+}
+
+function setTranslateX(px) {
+   element.style.transform = 'translateX(' + px + 'px)'
+}
+
+function startAnimation() {
+    goToRight()
 }
